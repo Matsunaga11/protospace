e20404f1df2e0bbd2ca965fb1875be9a71539fe7
Masahiro Iitomi <lpr8f81230@gmail.com>
2017-11-15 17:49:44 +0900

    ADD Likes Function

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,3 +1,19 @@
 class LikesController < ApplicationController
+  def create
+    @like = Like.new(
+      user_id: current_user.id,
+      prototype_id: params[:prototype_id]
+      )
+    @like.save
+    redirect_to prototype_path(params[:prototype_id])
+  end
 
+  def destroy
+    @like = Like.find_by(
+      user_id: current_user.id,
+      prototype_id: params[:prototype_id]
+      )
+    @like.destroy
+    redirect_to prototype_path(params[:prototype_id])
+  end
 end
