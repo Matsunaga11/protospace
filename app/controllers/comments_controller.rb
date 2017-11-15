d2df069d0da69dabafb96567869f502e01cd9560
iriesatoshi <hirodai10073104@yahoo.co.jp>
2017-11-15 18:00:53 +0900

    コメント機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,35 @@
 class CommentsController < ApplicationController
   def create
-    @comment = Comment.create(text: params[:text], prototype_id: params[:prototype_id], user_id: current_user.id)
-    redirect_to "/prototypes/#{@comment.prototype.id}"
+    if user_signed_in?
+      @comment = Comment.create(text: params[:text], prototype_id: params[:prototype_id], user_id: current_user.id)
+      respond_to do |format|
+        format.html { redirect_to "/prototypes/#{@comment.prototype.id}" }
+        format.json
+      end
+    end
+  end
+
+  def destroy
+      @comment = Comment.find(params[:id])
+    if user_signed_in? && current_user.id == @comment.user.id
+      @comment.destroy
+      redirect_to "/prototypes/#{@comment.prototype.id}"
+    end
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+    if user_signed_in? && current_user.id == @comment.user.id
+      @prototype = Prototype.find(params[:prototype_id])
+    end
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    if user_signed_in? && current_user.id == @comment.user.id
+      @comment.update(comment_params)
+      redirect_to "/prototypes/#{@comment.prototype.id}"
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'prototypes#index'
 
   resources :prototypes do
-    resources :comments, only: [:create]
+    resources :comments, only: [:create, :destroy, :edit, :update]
   end
   resources :users, only: [:show, :edit, :update]
 end
